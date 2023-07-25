10b1a497e1345adb753afe6b4b38fc3cedf435d2
SilverB0w <jhin11tjz@gmail.com>
2023-07-25 09:57:57 +0800

    update BLOG

--- .history/data/siteMetadata_20230722102903.js
+++ .history/data/siteMetadata_20230722102903.js
@@ -2,7 +2,7 @@ const siteMetadata = {
     title: '银月长廊',
     author: '隽敏',
     authorImg: 'https://cdn.jsdelivr.net/gh/SilverB0w/SM-image@main/avatar.jpg',
-    description: '隽敏的个人博客，一处小小自留地，记录我在蓝星上的日常。',
+    description: '隽敏的个人博客，一处小小自留地，记录我在蓝星上的日常',
     theme: 'system', // system, dark or light
     lang: 'zh-Hans-CN',
 }
